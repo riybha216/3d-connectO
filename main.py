7a84a6a9cd6565983cd6ccfe03272fb11492ad2b
Riya Bhatia <45548395+riybha216@users.noreply.github.com>
2022-12-04 16:56:44 -0500

    everything works.

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 """
-To meet MVP:
-- [DONE] make 3d board w/ cells
-- make adjustable by height [make an input height, maybe by levels?]
----> easy, medium, hard [4, 6, 8] by height
----> connect4, 6, 8, and put a [custom] button too
-- [DONE] change player (red, yellow)
-- [DONE] make it recognize where you click
-- [DONE] add place to click for cube and allow the cube to drop
---> [DONE] stop cube when it reaches target square
-- [DONE] if player clicks on cell, a cube pops up on cell
-- [DONE] represent as a 3d list
-- do win detection
-- highlight whos turn it is
-- [DONE] do oop (maybe)
-
 tp3 features:
 - if have time: blue cube is like wild card
 - if have time: add rotation
 - if have time: fix minimax
-- [bad idea] if have time: can claim cubes below the grid (random num of cubes)
 """
 
 from cmu_112_graphics import *
@@ -39,6 +23,7 @@ class ThreeDBoard:
         self.currRow = None
         self.currCol = None
 
+    # gets the four cell points for each cell in the grid
     def getCellBounds(self, row, col):
         self.currRow = row
         self.currCol = col
@@ -56,6 +41,7 @@ class CreateCube:
         self.points = []
         self.height = 30
 
+    # calculates all vertices of the cube
     def getAllPoints(self):
         topFacePoints = copy.copy(self.bottomFaceVertices)
         self.points.append(self.bottomFaceVertices)
@@ -63,9 +49,9 @@ class CreateCube:
             if idx % 2 == 1:
                 topFacePoints[idx] -= self.height
         self.points.append(topFacePoints)
-        print(self.points)
         return self.points
 
+# checks all possible ways to win (across levels and on the grid itself)
 class WinDetection:
     def __init__(self, boards, numGrids):
         self.boards = boards
@@ -105,26 +91,27 @@ class WinDetection:
         return False
 
     def detectVerticalAcrossGrids(self):
-        """row = None
-        temp = []
-        allCols = self.getColumns()
-        for row in allCols[-1]:
-            for i in row:
-                if row[i] != None:
-                    row = i
-                else:
-                    break
-        for col in row:
-                print(col)
-                if allCols[0][row][col] != None:
-                    tempSet.add([allCols[0][row][col]])
-                    r, c = row, col
-        for grid in range(len(allCols)):
-            tempSet.add()
-        if len(tempSet) == 1:
+        lis = []
+        for board in self.boards:
+            for row in board.board:
+                lis.append(row)
+        # idea to flatten list using list comprehension from
+        # Practice Midterm 2 Review Session on Panopoto, 112 Course Staff
+        newL = [e for sList in lis for e in sList]
+        print(newL)
+        anotherL = list()
+        for e in newL:
+            if e != None:
+                anotherL.append(newL.index(e))
+                newL[newL.index(e)] = None
+        
+        if len(anotherL) > 0:
+            for element in anotherL:
+                if element + ((self.numGrids - 1) ** 2) not in anotherL:
+                    return False
             return True
         else:
-            return False"""
+            return False
 
     def detectPositiveDiagonalonGrid(self):
         temp = []
@@ -132,10 +119,21 @@ class WinDetection:
             for i, row in enumerate(board.board):
                 temp.append(row[i])
             if temp[0] != None and len(set(temp)) == 1: return True
+            temp.clear()
         return False
 
     def detectPositiveDiagonalAcrossGrids(self):
-        return
+        for board in self.boards:
+            print(board.board)
+        if self.boards[2].board[0][0] == None:
+            return False
+        else:
+            if len(self.boards) > 2:
+                if (self.boards[2].board[0][0] == self.boards[1].board[1][1] == 
+                    self.boards[0].board[2][2]):
+                    return True
+                else:
+                    return False
 
     def detectNegativeDiagonalonGrid(self):
         temp = []
@@ -143,17 +141,17 @@ class WinDetection:
             for i, row in enumerate(board.board[::-1]):
                 temp.append(row[i])
             if temp[0] != None and len(set(temp)) == 1: return True
+            temp.clear()
         return False
 
     def detectNegativeDiagonalAcrossGrids(self):
         diagonalPoints = []
-        board = self.boards[0]
+        board = self.boards[self.numGrids - 1]
         if board.board[board.cols-1][0] == None:
             return False
         for board in self.boards:
             for i in range(board.rows):
                 diagonalPoints.append(board)
-
 
     def checkWin(self):
         if (self.detectHorizontalOnGrid() or 
@@ -164,6 +162,7 @@ class WinDetection:
             self.detectPositiveDiagonalAcrossGrids() or 
             self.detectNegativeDiagonalonGrid() or
             self.detectNegativeDiagonalAcrossGrids()):
+            print("True")
             return True
         return False
 
@@ -180,12 +179,14 @@ def appStarted(app):
     app.page = 0
     app.margin = 50
 
+# switches the color of the player
 def switchPlayers(app):
     if app.players.index(app.currPlayer) == 0:
         app.currPlayer = app.players[1]
     else:
         app.currPlayer = app.players[0]
 
+# generates boards at the appropriate height for each specific level
 def generateBoards(app, num):
     for i in range(num + 1):
         if num <= 3:
@@ -195,6 +196,8 @@ def generateBoards(app, num):
         elif num == 5:
             app.boards.append(ThreeDBoard(app.width-100, app.height, num, num, app.marginMultiplier + (1.5 * i)))
 
+# checks if the user has clicked the appropriate point on the grid, and if
+# the click itself is on the grid
 def pointInGrid(app, x, y):
     pointsList = []
     counter = 0
@@ -212,6 +215,8 @@ def pointInGrid(app, x, y):
             return (r, c)
         counter += 1
 
+# finds points that have already been clicked so cubes cannot be placed on
+# those points again
 def getClickedPoints(app):
     pointList = []
     for board in app.boards:
@@ -220,6 +225,7 @@ def getClickedPoints(app):
                 if board.board[row][col] != None:
                     pointList.append(board.getCellBounds(row, col))
 
+# checks which point on the topmost grid has been clicked
 def checkForOrange(app):
     firstBoard = app.boards[0]
     for row in range(firstBoard.rows):
@@ -231,8 +237,10 @@ def checkForOrange(app):
 
 def mousePressed(app, event):
     x, y = event.x, event.y
-    num = 1
+    if pointToHelp(app, x, y):
+        app.page = 4
     if app.page == 0:
+        app.boards.clear()
         if pointToLevelOne(app, x, y):
             app.page = 1
             app.numGrids = 3
@@ -241,12 +249,13 @@ def mousePressed(app, event):
             app.page = 2
             app.numGrids = 4
             generateBoards(app, app.numGrids)
-            print(app.boards)
         elif pointToLevelThree(app, x, y):
             app.page = 3
             app.numGrids = 5
             generateBoards(app, app.numGrids)
     elif app.page in {1, 2, 3}:
+        if pointToExit(app, x, y):
+            app.page = 0
         app.bottomFacePoints.clear()
         if pointInGrid(app, event.x, event.y) == None:
             app.selection = (-1, -1)
@@ -254,7 +263,11 @@ def mousePressed(app, event):
             (row, col) = pointInGrid(app, event.x, event.y)
             app.selection = (row, col)
             checkForOrange(app)
+    elif app.page == 4:
+        if pointToExit(app, x, y):
+            app.page = 0
 
+# checks if the cube has reached the appropriate (x, y) coordinates
 def reached(app):
     firstBoard = app.boards[0]
     (row, col) = (None, None)
@@ -264,7 +277,6 @@ def reached(app):
             if firstBoard.board[r][c] == "orange":
                 (row, col) = (r, c)
                 break
-    
     possibleBoards = [i+1 for i in range(app.numGrids)]
     for boardNum in possibleBoards:
         if app.boards[boardNum].board[row][col] == None:
@@ -279,14 +291,14 @@ def reached(app):
             app.boards[boardToBeReached].board[row][col] = app.currPlayer
             app.bottomFacePoints.clear()
             switchPlayers(app)
-            return (row, col, boardToBeReached, True)
-    return (row, col, boardToBeReached, False)
+            return True
+    return False
 
 def timerFired(app):
     if len(app.bottomFacePoints) == 0:
         return
     else:
-        (row, col, boardToBeReached, hasReached) = reached(app)
+        hasReached = reached(app)
         if not hasReached:
             points = app.bottomFacePoints[0]
             for idx in range(len(points)):
@@ -294,6 +306,7 @@ def timerFired(app):
                     points[idx] += app.dy
         return
 
+# finds if the click has coordinates that are within the bounds of the buttons
 def pointToLevelOne(app, x, y):
     return (app.width//2-80 <= x <= app.width//2+80 and
             app.height//2 - (2 * app.margin) <= y <= app.height // 2 - app.margin)
@@ -309,6 +322,11 @@ def pointToLevelThree(app, x, y):
 def pointToExit(app, x, y):
     return (app.width // 2 - app.margin <= x <= app.width // 2 + app.margin and 
             app.height - 20 <= y <= app.height)
+
+def pointToHelp(app, x, y):
+    return (20 <= x <= 100 and 20 <= y <= 40)
+
+# draws individual pieces, screens, and boards
 
 def drawMovingCube(app, canvas):
     fill = app.currPlayer
@@ -368,8 +386,22 @@ def drawExit(app, canvas):
     canvas.create_text(app.width//2, app.height-10, text="Exit")
 
 def drawHumanHumanGame(app, canvas):
+    drawExit(app, canvas)
+    currFill, otherFill = None, None
     canvas.create_rectangle(20, 20, 100, 40, fill="black")
     canvas.create_text(60, 30, text="Help", fill="white", font="Arial 15 bold")
+
+    if app.currPlayer == app.players[0]:
+        currFill = app.currPlayer
+        otherFill = "black"
+    else:
+        currFill = "black"
+        otherFill = app.currPlayer
+
+    canvas.create_text(app.width//2 - 100, 30, text="Player 1", font="Arial 16 bold",
+                        fill = currFill)
+    canvas.create_text(app.width//2 + 100, 30, text="Player 2", font="Arial 16 bold",
+                        fill = otherFill)
 
     for board in app.boards:
         for row in range(board.rows):
@@ -408,15 +440,50 @@ def drawGameOver(app, canvas):
     canvas.create_text(app.width//2, app.height/2, text="{} wins!".format(player), fill="black", 
                       font="Arial 22 bold")
 
+def drawExit(app, canvas):
+    canvas.create_rectangle(app.width // 2 - app.margin, app.height - 20,
+                            app.width // 2 + app.margin, app.height, fill="green")
+
+    canvas.create_text(app.width//2, app.height-10, text="Exit")
+
 def drawHelpScreen(app, canvas):
-    return
+    canvas.create_rectangle(0, 0, app.width, app.height, fill="light blue")
+    canvas.create_text(app.width//2, 40, text="Game Rules", font="Arial 20 bold",
+                        fill="black")
+    canvas.create_text(app.width//2, 70, text="ConnectO is a two-player game.", font="Arial 18",
+                        fill="black")
+    canvas.create_text(app.width//2, 90, text="The goal is to be the first to place your pieces in a row, column, or diagonal.", font="Arial 18",
+                        fill="black")
+    canvas.create_text(app.width//2, 110, text="However, since the game is in 3D, you can align your pieces and win in 2D or 3D!", font="Arial 18",
+                        fill="gray")
+
+    canvas.create_text(app.width//2, 160, text="TO PLAY:", font="Arial 30 bold",
+                        fill="black")
+    canvas.create_text(app.width//2, 180, text="Click on the topmost board to generate pieces.", font="Arial 18",
+                        fill="black")
+    canvas.create_text(app.width//2, 200, text="Pieces will fall to their respective slots based on available space.", font="Arial 18",
+                        fill="black")
+    canvas.create_text(app.width//2, 220, text="The player whose turn it currently is will be highlighted at the top of the screen.", font="Arial 18",
+                        fill="black")
+    canvas.create_text(app.width//2, 250, text="There are three levels: easy, medium, and hard.", font="Arial 20 bold",
+                        fill="black")
+    canvas.create_text(app.width//2, 270, text="For easy mode, you win by connecting three pieces in any dimension.", font="Arial 18",
+                        fill="black")
+    canvas.create_text(app.width//2, 290, text="For medium and hard, you win by connecting four and five pieces, respectively.", font="Arial 18",
+                        fill="black")
+            
+    canvas.create_text(app.width//2, 330, text="You can also click the green Exit screen at the bottom to go back to Home.", font="Arial 18",
+                        fill="black")
+    canvas.create_text(app.width//2, 360, text="Now, think carefully, and have fun! :)", font="Arial 25 bold",
+                        fill="black")
+    drawExit(app, canvas)
 
 def redrawAll(app, canvas):
     if app.page == 0:
         drawHomeScreen(app, canvas)
-        drawHelpScreen(app, canvas)
     elif app.page in {1, 2, 3}:
         drawHumanHumanGame(app, canvas)
+    elif app.page == 4:
         drawHelpScreen(app, canvas)
 
 runApp(width=700, height=700)
